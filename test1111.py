5b7df4c279897a0dce6f3694a0b199b305f1bc34
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-08-05 17:08:17 -0700

    'twenty'

---
+++ test1111.py
@@ -0,0 +1,3 @@
+with open('test.txt', 'r') as file:
+    data = file.read().rstrip()
+print(data)
--- texttospeech.py
+++ texttospeech.py
@@ -4,15 +4,17 @@ translator = Translator()
 # This module is imported so that we can 
 # play the converted audio
 import os
-  
+with open('test.txt', 'r') as file:
+    mytext_origin = file.read().rstrip() 
+
 # The text that you want to convert to audio
 # print("Insert your text:")
 # mytext_origin = input()
-mytext_origin = 'Deci, la fiecare 5 minute, botul va introduce acest link și va reîmprospăta datele.De fiecare dată când va introduce acest link și va prelua datele, trebuie să le salveze într-un fișier txt.'# here you can change the text
+# mytext_origin = 'Am nevoie de asta în 2 ore de acum înainte. nu licitați dacă nu îl puteți finaliza acum cât mai curând posibil. va rog sa vedeti poza atasata. este o captură de ecran a unui poster. Am nevoie de cineva care să recreeze designul și să-mi trimită fișierul de design în format pdf, ca să îl pot imprima. foarte usor! Am nevoie de asta în 2 ore de acum înainte.'# here you can change the text
 mytext = translator.translate(mytext_origin, dest='ro').text  
 # Language in which you want to convert
 language = 'ro'
-  
+print(mytext)
 # Passing the text and language to the engine, 
 # here we have marked slow=False. Which tells 
 # the module that the converted audio should 
